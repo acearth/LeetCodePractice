9b3c3c8f8f15891f5e246f8002cee49aced530d9
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-07-16 21:51:00 +0800

    add tranverse method for listNode
    reverse listNode reverse in k-group is resolved

--- base/ListNode.rb
+++ base/ListNode.rb
@@ -1,7 +1,22 @@
- class ListNode
-     attr_accessor :val, :next
-     def initialize(val)
-         @val = val
-         @next = nil
-     end
- end
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+
+  def to_s
+    return "#{@val}"
+  end
+
+  def tranverse
+    p @val
+    if self.next!=nil
+      self.next.tranverse
+    end
+  end
+
+end
+
+
