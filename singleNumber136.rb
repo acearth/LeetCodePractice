8df16d68109f75722ec65b8522ac431502f565bd
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-07-19 10:20:05 +0800

    modified single number. solved by XOR but the time is no significant decrease

--- singleNumber136.rb
+++ singleNumber136.rb
@@ -1,23 +1,8 @@
 def single_number(nums)
-  a=Hash.new
-  for element in nums do
-    if a[element]==nil
-      a[element]="one"
-    else
-      a[element]="two"
-    end
-  end
-  v=-88
-  for key in a.keys do
-    if a[key]=="one"
-      v=key
-      break
-    end
-  end
-  return v
+  a=0
+  nums.each{ |i| a^=i}
+  a
 end
 
-nu=[1,2,3,2]
-k=single_number(nu)
-p k
-
+nu=[1,2,3,2,1]
+p single_number nu
