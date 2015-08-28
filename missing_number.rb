745e13c5025c0a2a81b45d508ac4d2c122e12d47
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-08-28 14:19:40 +0800

    missing number retreated

--- missing_number.rb
+++ missing_number.rb
@@ -1,15 +1,3 @@
 def missing_number(nums)
-  nums.each { |n| place(nums, n) if nums[n]!=n }
-  nums.each_with_index { |n, i| return i if i!=n }
-  nums.size
+  (1..nums.size).reduce(:^) ^ nums.reduce(:^)
 end
-
-def place(nums, n)
-  if nums[n]!=nil && nums[n]!=n
-    t=nums[n]
-    nums[n]=n
-    place(nums, t)
-  end
-end
-
-
