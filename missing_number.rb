1612838d3541e4955962c9608f9a0d2b5c0b225b
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-08-30 09:14:57 +0800

    missing number retreated

--- missing_number.rb
+++ missing_number.rb
@@ -1,3 +1,4 @@
 def missing_number(nums)
-  (1..nums.size).reduce(:^) ^ nums.reduce(:^)
+  #(1..nums.size).reduce(:^) ^ nums.reduce(:^)
+  nums.each_with_index.map{|n,i| n^(i+1)}.inject{|result,i| result^=i }
 end
