d8bdbe8636d5fd63ddda9e9b955994e41657e0ae
Arthur (AN.Xq) <arthur4it@gmail.com>
2015-09-10 12:47:28 +0800

    Update powXn.rb

--- powXn.rb
+++ powXn.rb
@@ -3,12 +3,5 @@ def my_pow(x,n)
   return 1 if n==0
   return x if n==1
   v=my_pow(x,n/2)
-  return v*v*x if n%2==1
-  return v*v
+  v*v*my_pow(x,n%2)
 end
-
-p my_pow(2,3)
-p my_pow(2,-3)
-p my_pow 0.44894,-5
-p my_pow 0.44894,5
-
