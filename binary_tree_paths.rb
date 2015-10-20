99601bc581f4902fe4443be97ec55f73f6d99785
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-10-20 08:45:02 +0800

    morning commitment

--- binary_tree_paths.rb
+++ binary_tree_paths.rb
@@ -1,28 +1,7 @@
-require './base/TreeNode'
 def binary_tree_paths(tree)
-  result=[]
-  return [] if tree==nil
-  dfs(tree,tree.val.to_s,result)
-  result
+  return [] if not tree
+  res=binary_tree_paths(tree.left) + binary_tree_paths(tree.right)
+  return ["#{tree.val}"] if res.size==0
+  prefix="#{tree.val}->"
+  res.map{|sub| prefix+sub }
 end
-
-def dfs(tree,ancestors,result)
-  result<<ancestors if tree.left==nil && tree.right==nil
-  if tree.left!=nil
-    neoAncestors=ancestors.dup+"->"+tree.left.val.to_s
-    dfs(tree.left,neoAncestors,result)
-  end
-  if tree.right!=nil
-    neoAncestors=ancestors.dup+"->"+tree.right.val.to_s
-    dfs(tree.right,neoAncestors,result)
-  end
-end
-
-t1=TreeNode.new(1)
-t2=TreeNode.new(2)
-t3=TreeNode.new(3)
-t1.left=t2
-t1.right=t3
-p binary_tree_paths(t1)
-
-
--- range_bitwise_and.rb
+++ range_bitwise_and.rb
@@ -1,12 +1,6 @@
 def range_bitwise_and(m,n)
-  return 0 if m==0
-  pre=m&n
-  return pre if m==n
-  b=(n-m).to_s(2)
-  kStr='1'
-  b.length.times { kStr+='0'}
-  k=kStr.to_i(2)
-  c=2**31-k
-  pre&=c
+  range=n-m
+  mask,k = -1, 1
+  (mask=mask<<1; k=k<<1) while k<range
+  n & m & mask
 end
-
