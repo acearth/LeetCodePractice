59f9831e2e4b453c3b9c9a3fdfff3c68168172f4
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-11-20 23:32:05 +0800

    divide without /*%

--- divide_Without_divide.rb
+++ divide_Without_divide.rb
@@ -1,29 +1,18 @@
-# @param {Integer} dividend
-# @param {Integer} divisor
-# @return {Integer}
 def divide(divident, divisor)
-  result=divideAct(divident, divisor)
-  return -2147483648 if result<-2147483648
-  return 2147483647 if result>2147483647
-  return result
+  res=go_divide(~divident+1, ~divisor+1) if divident<0 && divisor < 0
+  res=(~go_divide(divident, ~divisor+1)) + 1 if divident>=0 && divisor<0
+  res=(~go_divide(~divident+1, divisor)) + 1 if divident<0 && divisor>0
+  res=go_divide(divident, divisor) if divident>=0 && divisor>0
+  (-2147483648..2147483647).include?(res) ? res : 2147483647
 end
 
-def divideAct(dividend, divisor)
-  raise RuntimeError if divisor==0
-  return divideAct(-dividend, -divisor) if divisor<0&&dividend<0
-  return -1*divideAct(-dividend, divisor) if dividend<0&&divisor>0
-  return -1*divideAct(dividend, -divisor) if dividend>0&&divisor<0
-  return 0 if dividend<divisor
-  kd=dividend
-  kr=0
-  d=0
-  while true
-    break if kr+kr>kd
-    kr+=kr
-    d+=d
-    kr=divisor if kr==0
-    d=1 if d==0
+def go_divide(divident, divisor)
+  return 0 if divident < divisor
+  return divident if divisor==1
+  res, cur=1, divisor
+  while cur+cur < divident
+    res=res<<1
+    cur=cur<<1
   end
-  return d if kd-kr<divisor
-  return d+divideAct(kd-kr, divisor)
+  res + divide(divident - cur, divisor)
 end
