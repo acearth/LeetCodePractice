2e01534f77f47d1836a96ce57e330c7b6ec41f67
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-11-20 09:12:53 +0800

    first bad version retreated

--- first_bad_version.rb
+++ first_bad_version.rb
@@ -1,11 +1,4 @@
+# @NOTE: this bsearch() is in find-min mode
 def first_bad_version(n)
-    is_bad_interval(1,n+1)    
-end
-
-def is_bad_interval(left, right)
-    return left if left+1==right
-    m=left+(right-left)/2
-    return m if is_bad_version m and not is_bad_version m-1
-    return is_bad_interval(left, m) if is_bad_version m   
-    is_bad_interval(m+1, right)
+  (1..n).bsearch{ |i| is_bad_version(i) }
 end
