26f38286ee1795b76e5758f4ed4b6162d129ce9a
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-11-26 19:16:58 +0800

    retreated title to number in ONE line

--- title_to_number_Excel_table.rb
+++ title_to_number_Excel_table.rb
@@ -1,5 +1,3 @@
-# recursive method run in 140ms, too slow
 def title_to_number(str)
-  return 0 if str.length==0
-  title_to_number(str[0..-2])*26 + str[-1].ord-64
+    str=="" ? 0 : title_to_number(str[0..-2])*26 + str[-1].ord-64
 end
