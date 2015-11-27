32aa678963ba5d5e3376e7e895a7d9a987fd0e2e
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-11-27 23:08:17 +0800

    retreated again

--- Queue_By_stacks.rb
+++ Queue_By_stacks.rb
@@ -5,7 +5,7 @@ class Queue
 
   def push x; @in<<x end
 
-  def pop
+  def pop    ## This method should NOT return value but it does.
     ( @out<< @in.pop while @in.size > 0 ) if @out.empty?
     @out.pop
   end
