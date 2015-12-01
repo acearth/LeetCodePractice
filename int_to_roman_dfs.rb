5687eb7230b09a2d42212571c5f1c089e712a339
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-12-01 15:39:41 +0800

    int to roman retreated by hash recursively but slow

--- int_to_roman_dfs.rb
+++ int_to_roman_dfs.rb
@@ -1,26 +1,8 @@
-def int_to_roman(num)
-  dfs(num,"")
-end
-def dfs(val,tmpStr)
-  case val
-  when 1000..3999 then dfs(val-1000,tmpStr+'M')
-  when 900..999 then dfs(val-900,tmpStr+'CM')
-  when 500..899 then dfs(val-500,tmpStr+'D')
-  when 400..499 then dfs(val-400,tmpStr+'CD')
-  when 100..399 then dfs(val-100,tmpStr+'C')
-  when 90..99 then dfs(val-90,tmpStr+'XC')
-  when 50..89 then dfs(val-50,tmpStr+'L')
-  when 40..49 then dfs(val-40,tmpStr+'XL')
-  when 10..39 then dfs(val-10,tmpStr+'X')
-  when 9 then return tmpStr+'IX'
-  when 5..8 then dfs(val-5,tmpStr+'V')
-  when 4 then return tmpStr+'IV'
-  when 1..3 then dfs(val-1,tmpStr+'I') 
-  when 0  then tmpStr
-  end
-end
+TABLE=[[1000...4000,'M'],[900...1000,'CM'],[500...900,'D'], [400...500,'CD'],
+       [100...400,'C'],[90...100,'XC'],[50...90,'L'],[40...50,'XL'],
+       [10...40,'X'],[9...10,'IX'],[5...9,'V'],[4...5,'IV'],[1...4,'I']].to_h
 
-p int_to_roman 1
-p int_to_roman 1999
-p int_to_roman 1880
-
+def int_to_roman(num,cur="")
+  TABLE.each{|k,v| return int_to_roman(num-k.begin, cur+v) if k.include? num }
+  cur
+end
