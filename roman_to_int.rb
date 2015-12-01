e0769dbd90cb014b17e6166be9db4e8437868b64
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-12-01 14:37:55 +0800

    roman to int redo again

--- roman_to_int.rb
+++ roman_to_int.rb
@@ -1,17 +1,10 @@
+VALUE=[['I', 1], ['V', 5], ['X', 10], ['L', 50],
+       ['C', 100], ['D', 500], ['M', 1000]].to_h
 def roman_to_int(str)
   return 0 if str==""
   %w[M D C L X V I].each do |ch|
     next if !str.include? ch
     ci = str.index(ch)
-    return roman_to_int(str[ci+1..-1]) - roman_to_int(str[0...ci]) +
-      case str[ci]
-    when 'M' then 1000
-    when 'D' then 500
-    when 'C' then 100
-    when 'L' then 50
-    when 'X' then 10
-    when 'V' then 5
-    else 1
-    end
+    return VALUE[ch] + roman_to_int(str[ci+1..-1]) - roman_to_int(str[0...ci])
   end
 end
