747d0dc6581b454c5d1d909f41f20cdc422e7a47
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-12-03 12:48:39 +0800

    concised solution

--- level_order_traversal.rb
+++ level_order_traversal.rb
@@ -1,11 +1,9 @@
 def level_order(root)
   return [] if not root
-  result, next_queue = [], [root]
-  loop do
-    queue=next_queue
-    break if queue.size==0
-    result<<queue.map(&:val)
-    next_queue = queue.map{|node| [node.left,node.right]}.flatten.compact
+  queue, result = [root], []
+  while queue.size > 0
+    result << queue.map(&:val)
+    queue = queue.map { |node| [node.left, node.right] }.flatten.compact
   end
   result
 end
