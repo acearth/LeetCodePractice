8e7da9735f96c18ae4e9435e92371462c99117a1
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-12-24 23:40:54 +0800

    retreated but slow, need do more

--- count_and_say.rb
+++ count_and_say.rb
@@ -1,25 +1,14 @@
-# @param {Integer} n
-# @return {String}
+def count(str)
+  parts = ['']
+  str.chars.each { |ch| ch == parts[-1][-1] ? parts[-1] << ch : parts << ch }
+  parts.shift
+  parts.map(&:length).zip(parts.map { |s| s[0] }).join
+end
+
 def count_and_say(n)
-  base='1'
-  (n-1).times do
-    base=parse(base)
-  end
-  base
+  state="1"
+  (n-1).times { state=count(state) }
+  state
 end
 
-def parse(str)
-  return "" if str.size<1
-  c=0
-  str.length.times do |i|
-    c+=1 if str[i]==str[0]
-    break if str[i]!=str[0]
-  end
-  return c.to_s+str[0]+parse(str[c..-1])
-end
-
-p parse ('1')
-p parse ('123')
-p parse('1122233312') =='2132331112'
-
-p count_and_say 3
+# NOTE: Try to 1) get result recursively and 2) avoid repeated computation
