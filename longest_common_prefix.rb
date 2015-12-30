88c3e26a5cf0ab81013b4be321a0870c0f9b98a6
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2015-12-30 19:59:53 +0800

    do again

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -1,29 +1,9 @@
-
-def longest_common_prefix(strings)
-  prefix=""
-  return prefix if strings==nil||strings[0]==nil
-  size=strings.length
-  return strings[0] if size==1
-  tmpl=strings[0]
-  tmpl.length.times do |i|
-    l=0
-    for j in 1..size-1
-      break if strings[j][i]==nil
-      break if strings[j][i]!=tmpl[i]
-      l+=1
-    end
-    if size==l+1
-      prefix+=tmpl[i]
-    else
-      break
+def longest_common_prefix(strs)
+  prefix = strs[0]
+  strs.each do |str|
+    prefix.size.times do |i|
+      (prefix= prefix[0...i]; break) if prefix[i] != str[i]
     end
   end
-  return prefix
+  prefix ||= ""
 end
-
-st=["",""]
-st=["abc","abcd"]
-p longest_common_prefix st
-st=["",""]
-p longest_common_prefix st
-
