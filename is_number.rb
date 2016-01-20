0345835aae982356158341620888ca91621f1c4e
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-01-21 00:04:24 +0800

    is_number_expressiong solved by dfa

--- is_number.rb
+++ is_number.rb
@@ -4,34 +4,44 @@ def process(state, str)
   case state
   when 0
     return process(1, remain) if /[+-]/ =~ current
-    return process(2, remain) if /0/ =~ current
-    return process(3, remain) if /[1-9]/ =~ current
-    return process(4, remain) if /\./ =~ current
+    return process(2, remain) if /\./ =~ current
+    return process(4, remain) if /\d/ =~ current
   when 1
-    return process(2, remain) if /0/ =~ current
-    return process(3, remain) if /[1-9]/ =~ current
+    return process(2, remain) if /\./ =~ current
+    return process(4, remain) if /\d/ =~ current
   when 2
-    return process(4, remain) if /\./ =~ current
+    return process(3, remain) if /\d/ =~ current
   when 3
-    return process(3, remain) if /[0-9]/ =~ current
-    return process(4, remain) if /\./ =~ current
-    return process(6, remain) if /e/ =~ current
+    return process(3, remain) if /\d/ =~ current
+    return process(7, remain) if /e/ =~ current
   when 4
-    return process(5, remain) if /[0-9]/ =~ current
+    return process(4, remain) if /\d/ =~ current
+    return process(5, remain) if /\./ =~ current
+    return process(7, remain) if /e/ =~ current
   when 5
-    return process(5, remain) if /[0-9]/ =~ current
+    return process(6, remain) if /\d/ =~ current
+    return process(7, remain) if /e/ =~ current
   when 6
-    return process(7, remain) if /[+-]/ =~ current
-    return process(8, remain) if /[0-9]/ =~ current
+    return process(6, remain) if /\d/ =~ current
+    return process(7, remain) if /e/ =~ current
   when 7
-    return process(8, remain) if /[0-9]/ =~ current
+    return process(8, remain) if /[+-]/ =~ current
+    return process(9, remain) if /\d/ =~ current
   when 8
-    return process(8, remain) if /[0-9]/ =~ current
+    return process(9, remain) if /\d/ =~ current
+  when 9
+    return process(9, remain) if /\d/ =~ current
   end
   false
 end
-
 def is_number(s)
-  state = process(0,s.strip)
-  [2,3,5,8].include? state
+  [3,4,5,6,9].include? process(0, s.strip)
 end
+p is_number(" 0.1  ")
+p is_number("2e10")
+p is_number("+.8")
+p is_number(".8")
+p is_number("46.e8")
+p is_number(".2e81")
+p is_number("abc")==false
+p is_number("1 a")==false
