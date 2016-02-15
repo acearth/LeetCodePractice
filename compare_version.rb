bd461d1bd3cddc14110a93fee5d0562e15c1c10e
Arthur <arthur4it@gmail.com>
2016-02-15 10:16:50 +0800

    use detect
    TODO: deal with padding zeros

--- compare_version.rb
+++ compare_version.rb
@@ -1,11 +1,7 @@
 def compare_version(v1,v2)
   v1 = v1.split(".").map(&:to_i)
   v2 = v2.split(".").map(&:to_i)
-  (v1.size - v2.size).times { v2<<0 }
-  (v2.size - v1.size).times { v1<<0 }
-  v1.size.times do |i|
-    t= v1[i]<=>v2[i]
-    return t if t!=0
-  end
-  0
+  (v1.size - v2.size).times { v2 << 0 }
+  (v2.size - v1.size).times { v1 << 0 }
+  v1.zip(v2).each.map { |(x, y)| x <=> y }.detect{ |n| n != 0 } || 0
 end
