a021d7df3e79567f8883c9ba5eb3bf8308cf051d
Arthur <arthur4it@gmail.com>
2016-02-15 10:20:10 +0800

    Update compare_version.rb

--- compare_version.rb
+++ compare_version.rb
@@ -3,5 +3,5 @@ def compare_version(v1,v2)
   v2 = v2.split(".").map(&:to_i)
   (v1.size - v2.size).times { v2 << 0 }
   (v2.size - v1.size).times { v1 << 0 }
-  v1.zip(v2).each.map { |(x, y)| x <=> y }.detect{ |n| n != 0 } || 0
+  v1.zip(v2).each.map { | x, y | x <=> y }.detect{ |n| n != 0 } || 0
 end
