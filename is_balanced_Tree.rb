b3523b21200e2045ac7a96dce9fddb14a2c7237f
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-03-20 19:09:19 +0800

    retreat .

--- is_balanced_Tree.rb
+++ is_balanced_Tree.rb
@@ -2,11 +2,11 @@ def check(tree)
   return 0 if not tree
   left, right = check(tree.left), check(tree.right)
   raise if (left - right).abs > 1 # unbalanced
-  1 + [left, right].max # height of balanced tree
+  1 + [left, right].max # height
 end
 
 def is_balanced(root)
-  check(root) != false
+  check(root) && true
 rescue
   false
 end
