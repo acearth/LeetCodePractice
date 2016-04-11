b89e2f43a37016c56c2fbd9506bb9462f4152baa
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-04-11 22:57:17 +0800

    add NFA solution for valid number check

--- is_number.rb
+++ is_number.rb
@@ -1,3 +1,41 @@
+def is_number(str, state = 0)
+  return [2, 3, 4, 7].include? state if str && str.empty? || !state
+  case state
+  when 0
+    str.strip! # remove white space before take action
+    return true if is_number(str, 1)
+  when 1
+    return true if is_number(str, 8)
+  when 2
+    return true if is_number(str, 4)
+  when 3
+    return true if is_number(str, 4)
+  when 5
+    return true if is_number(str, 6)
+  end
+  next_state = TRANSIT[state][type(str[0])]
+  is_number(str[1..-1], next_state && next_state.first)
+end
+
+TRANSIT = [[[], [], [1], []], # 0
+           [[8], [], [], [2]], # 1
+           [[3], [], [], [2]], # 2
+           [[], [], [], [3]], # 3
+           [[], [5], [], [4]], # 4
+           [[], [], [6], []], # 5
+           [[], [], [], [7]], # 6
+           [[], [], [], [7]], # 7
+           [[], [], [], [4]]].freeze
+
+def type(char)
+  case char
+  when '.' then 0
+  when 'e' then 1
+  when /[+-]/ then 2
+  when /\d/ then 3
+  else 4 end # unacceptable input
+end
+
 def process(state, str)
   return state if str.empty?
   current, remain = str[0], str[1..-1]
@@ -34,7 +72,7 @@ def process(state, str)
   end
   false
 end
-def is_number(s)
+def is_number_dfa(s)
   [3,4,5,6,9].include? process(0, s.strip)
 end
 p is_number(" 0.1  ")
