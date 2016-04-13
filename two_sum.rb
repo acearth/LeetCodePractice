bc2f13b31a03ac0ec7006d4a1ca94cce1d4bba4e
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-04-13 13:02:03 +0800

    modify two_sum solution according to question demand, and use default assignment

--- two_sum.rb
+++ two_sum.rb
@@ -1,4 +1,3 @@
-def two_sum(nums, target)
-  have={}
-  nums.each_with_index{|n,i| have[target-n] ? (return [have[target-n],i+1]) : (have[n]=i+1) }
+def two_sum(nums, target, have = {})
+  nums.each_with_index { |n, i| have[k = target - n] ? (return [have[k], i]) : have[n] = i }
 end
