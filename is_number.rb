761a92c71dfe9cc8350c1b9fb7bb42fc8fa7266e
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-04-17 13:19:24 +0800

    modify implementation of is_number

--- is_number.rb
+++ is_number.rb
@@ -1,22 +1,3 @@
-def is_number(str, state = 0)
-  return [2, 3, 4, 7].include? state if str && str.empty? || !state
-  case state
-  when 0
-    str.strip! # remove white space before take action
-    return true if is_number(str, 1)
-  when 1
-    return true if is_number(str, 8)
-  when 2
-    return true if is_number(str, 4)
-  when 3
-    return true if is_number(str, 4)
-  when 5
-    return true if is_number(str, 6)
-  end
-  next_state = TRANSIT[state][type(str[0])]
-  is_number(str[1..-1], next_state && next_state.first)
-end
-
 TRANSIT = [[[], [], [1], []], # 0
            [[8], [], [], [2]], # 1
            [[3], [], [], [2]], # 2
@@ -27,53 +8,38 @@ TRANSIT = [[[], [], [1], []], # 0
            [[], [], [], [7]], # 7
            [[], [], [], [4]]].freeze
 
-def type(char)
-  case char
-  when '.' then 0
-  when 'e' then 1
-  when /[+-]/ then 2
-  when /\d/ then 3
-  else 4 end # unacceptable input
-end
-
-def process(state, str)
-  return state if str.empty?
-  current = str[0]
-  remain = str[1..-1]
+def check(input, state)
+  return [2, 3, 4, 7].include? state if input && input.empty? || !state
   case state
-  when 0
-    return process(1, remain) if /[+-]/ =~ current
-    return process(2, remain) if /\./ =~ current
-    return process(4, remain) if /\d/ =~ current
-  when 1
-    return process(2, remain) if /\./ =~ current
-    return process(4, remain) if /\d/ =~ current
-  when 2
-    return process(3, remain) if /\d/ =~ current
-  when 3
-    return process(3, remain) if /\d/ =~ current
-    return process(7, remain) if /e/ =~ current
-  when 4
-    return process(4, remain) if /\d/ =~ current
-    return process(5, remain) if /\./ =~ current
-    return process(7, remain) if /e/ =~ current
-  when 5
-    return process(6, remain) if /\d/ =~ current
-    return process(7, remain) if /e/ =~ current
-  when 6
-    return process(6, remain) if /\d/ =~ current
-    return process(7, remain) if /e/ =~ current
-  when 7
-    return process(8, remain) if /[+-]/ =~ current
-    return process(9, remain) if /\d/ =~ current
-  when 8
-    return process(9, remain) if /\d/ =~ current
-  when 9
-    return process(9, remain) if /\d/ =~ current
+    when 0
+      return true if check(input, 1)
+    when 1
+      return true if check(input, 8)
+    when 2
+      return true if check(input, 4)
+    when 3
+      return true if check(input, 4)
+    when 5
+      return true if check(input, 6)
   end
-  false
+  next_state = TRANSIT[state][input.first]
+  check(input[1..-1], next_state && next_state.first)
 end
 
-def is_number_dfa(s)
-  [3, 4, 5, 6, 9].include? process(0, s.strip)
+def is_number(str)
+  inputs = str.strip.chars.map do |char|
+    case char
+      when '.' then
+        0
+      when 'e' then
+        1
+      when /[+-]/ then
+        2
+      when /\d/ then
+        3
+      else
+        4
+    end # unacceptable input
+  end
+  check(inputs, 0)
 end
