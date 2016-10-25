5c69b5be70e09014cab246a197a67c1024c615e5
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-10-26 00:03:06 +0800

    retreated basic calculator

--- calculator_basic.rb
+++ calculator_basic.rb
@@ -1,55 +1,37 @@
 def calculate(s)
-  tokens=getExpression(s)
-  resultStack=[]
-  openStack=[]
-  tokens.each_with_index do |token, i|
-    if token=="("
-      resultStack<<token
-      openStack<<resultStack.size-1 ##position
-    elsif token==")"
-      cur=openStack.pop
-      lastValue=calculate_pure(resultStack[cur+1..-1])
-      resultStack=resultStack[0...cur]+[lastValue]
+  nums = [[]]
+  ops = [[]]
+  cur_num = -1
+  s.chars.each do |ch|
+    if ch.ord.between?(48, 57)
+      cur_num = 0 if cur_num == -1
+      cur_num = cur_num * 10 + ch.ord - 48 # ASCII code of '0' is 48
     else
-      resultStack<<token
+      nums.last << cur_num if cur_num > -1
+      cur_num = -1
     end
+    process_current(ch, nums, ops)
   end
-  calculate_pure(resultStack)
+  nums.last << cur_num if cur_num != -1
+  seq_eval(nums.pop, ops.pop)
 end
-def calculate_pure(arr)
-  cur=2
-  while cur<arr.size
-    if arr[cur-1]=='+'
-      arr[0]=arr[0]+arr[cur]
-    else
-      arr[0]=arr[0]-arr[cur]
-    end
-    cur+=2
+
+def process_current(ch, nums_stack, operator_stack)
+  if ch == '('
+    nums_stack << []
+    operator_stack << []
+  elsif ch == ')'
+    current = seq_eval(nums_stack.pop, operator_stack.pop)
+    nums_stack.last << current
+  elsif ch == '+' || ch == '-'
+    operator_stack.last << ch.to_sym
   end
-  arr[0]
 end
-def getExpression(str)
-  result=[]
-  tmp=nil
-  str.chars.each do |ch|
-    if /\d/=~ ch
-      tmp=0 if tmp==nil
-      tmp*=10
-      tmp+=ch.to_i
-    elsif /[\(|\)|+|-]/ =~ ch
-      result<<tmp if tmp!=nil
-      result<<ch
-      tmp=nil
-    end
-  end
-  result<<tmp if tmp!=nil
+
+def seq_eval(nums, ops)
+  result = nums.first
+  ops.length.times { |i| result = [result, nums[i + 1]].reduce(ops[i]) }
   result
 end
 
-s="1 + 1"
-p calculate(s)
-s=" 2-1 + 2 "
-p calculate(s)
-s="( 1+(4+5+2)-3)+(6+8)"
-p calculate(s)
-
+# p calculate("(1+(4+5+2)-3)+(6+8)") == 23
