50749dcd3054ed18816df21ba7e835b37b1b2bef
Arthur <arthur4it@gmail.com>
2016-10-27 21:08:57 +0800

    Update BSTIterator_class.rb

--- BSTIterator_class.rb
+++ BSTIterator_class.rb
@@ -1,33 +1,21 @@
-require './base/TreeNode'
 class BSTIterator
-  def initialize(root)
-    @stack=Array.new
-    while root!=nil
-      @stack.push root
-      root=root.left
+    def initialize(root)
+        @stack = []
+        add(root)
     end
-  end
 
-  def has_next
-    @stack.size>0
-  end
-
-  def next
-    nextNode=@stack.pop
-    p=nextNode.right
-    while p!=nil
-      @stack.push p
-      p=p.left
+    def has_next
+        @stack.any?
     end
-    nextNode.val
-  end
-end
-t1=TreeNode.new(1)
-t2=TreeNode.new(2)
-t3=TreeNode.new(3)
-t2.left=t1
-t2.right=t3
-iter=BSTIterator.new(t2)
-while iter.has_next
-  p iter.next
+
+    def next
+        node = @stack.pop
+        add(node.right)
+        node.val 
+    end
+    
+    private
+    def add(node)
+        @stack << node && node = node.left while node
+    end
 end
