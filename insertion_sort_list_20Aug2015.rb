a9c7598cb66e52a713e44ec04edc7ab01dd22ae7
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-10-30 22:17:19 +0800

    revised on 2016-10-30

--- insertion_sort_list_20Aug2015.rb
+++ insertion_sort_list_20Aug2015.rb
@@ -1,30 +1,16 @@
 def insertion_sort_list(head)
-  return head if head==nil || head.next==nil
-  p=head.next
-  head.next=nil
-  while p!=nil
-    pNext=p.next
-    p.next=nil
-    head=insert(head,p)
-    p=pNext
+  sorted = ListNode.new(0)
+  while head
+    node,node.next, head = head, nil, head.next
+    insert(sorted, node)
   end
-  head
+  sorted.next
 end
 
-def insert(sorted,target)
-  if target.val<sorted.val
-    target.next=sorted
-    return target
-  else
-    pre=sorted
-    q=pre.next
-    while q!=nil
-      break if q.val>target.val
-      pre=q
-      q=q.next
-    end
-    pre.next=target
-    target.next=q
-    sorted
-  end
+def insert(sorted, node)
+  p, value = sorted, node.val
+  p = p.next while p.next && p.next.val < value
+  pre_next = p.next
+  p.next = node
+  node.next = pre_next
 end
