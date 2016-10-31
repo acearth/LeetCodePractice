fa65a91c9b8a784192bae102122c57ea1e077c32
Arthur <arthur4it@gmail.com>
2016-10-31 10:46:56 +0800

    Update merge_two_lists_Sorted.rb

--- merge_two_lists_Sorted.rb
+++ merge_two_lists_Sorted.rb
@@ -1,7 +1,23 @@
-def merge_two_lists(l1,l2)
-  return l1 if not l2
-  return l2 if not l1
-  l1,l2 = l2, l1 if l1.val > l2.val  ## swap 
-  l1.next=merge_two_lists(l1.next,l2)
-  l1
+def merge_two_lists_recursively(list1, list2)
+  return list1 || list2 unless list1 && list2
+  list1, list2 = list2, list1 if list1.val > list2.val
+  list1.next = merge_two_lists(list1.next, list2)
+  list1
+end
+
+def merge_two_lists(l1, l2)
+  tail = sorted = ListNode.new(0)
+  while l1 && l2
+    if l1.val < l2.val
+      tail.next = l1
+      l1 = l1.next
+    else
+      tail.next = l2
+      l2 = l2.next
+    end
+    tail = tail.next
+  end
+  l1 = l2 if l2
+  tail.next = l1
+  sorted.next
 end
