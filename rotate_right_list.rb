5de77e34f6b356a8e45f541f3f8bad732c4c5143
Arthur <arthur4it@gmail.com>
2016-10-31 10:25:35 +0800

    Update rotate_right_list.rb

--- rotate_right_list.rb
+++ rotate_right_list.rb
@@ -1,27 +1,19 @@
-require './base/ListNode'
 def rotate_right(head, k)
-  return head if k==0 || head==nil|| head.next==nil
-  p=head
-  size=0
-  (size+=1; p=p.next ) while p!=nil
-  return head if k%size==0
-  step=size-k%size-1
-  newTail=head
-  step.times {newTail=newTail.next}
-  newHead=newTail.next
-  newTail.next=nil
-  adjacent=newHead
-  adjacent=adjacent.next while adjacent.next!=nil
-  adjacent.next=head
-  newHead
+  return head unless head
+  p, q = head, head
+  purify_k(head, k).times { p = p.next }
+  while p.next && q.next
+    p = p.next
+    q = q.next
+  end
+  p.next = head
+  head = q.next
+  q.next = nil
+  head
 end
-l1=ListNode.new(10)
-l2=ListNode.new(20)
-l3=ListNode.new(30)
-l4=ListNode.new(40)
-l5=ListNode.new(50)
-l1.next=l2
-l2.next=l3
-l3.next=l4
-l4.next=l5
-p rotate_right(l1,2).tranverse
+
+def purify_k(head, k)
+  len = 0
+  (head = head.next; len += 1) while head
+  k % len
+end
