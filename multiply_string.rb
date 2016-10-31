0951888be45e0b1a6a6a9e917e57ddcfa6e6f9db
Arthur <arthur4it@gmail.com>
2016-10-31 15:36:04 +0800

    Update multiply_string.rb

--- multiply_string.rb
+++ multiply_string.rb
@@ -1,8 +1,8 @@
 def multiply(num1, num2)
-  (get_int(num1.chars.map { |i| i.ord - 48 }) * get_int(num2.chars.map { |i| i.ord - 48 })).to_s
+  (integer_of(num1.chars.map { |i| i.ord - 48 }) * integer_of(num2.chars.map { |i| i.ord - 48 })).to_s
 end
 
-def get_int(digits)
+def integer_of(digits)
   result = 0
   digits.each do |d|
     result *= 10
