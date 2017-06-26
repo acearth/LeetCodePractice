cbd02514316e80bb1abcaf69b9aac17ab773a23d
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2017-06-26 23:26:19 +0800

    refactor add two numbers by recursive

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -1,21 +1,12 @@
-def add_two_numbers(l1,l2)
-  dummy=result=ListNode.new(0)
-  carry=0
-  while l1!=nil && l2!=nil
-    sum=l1.val+l2.val+carry
-    result.next=ListNode.new(sum%10)
-    result=result.next
-    carry=sum/10
-    l1, l2 =l1.next, l2.next
+def add_two_numbers(l1, l2)
+  dummy = result = ListNode.new(carry = 0)
+  while l1 && l2
+    tmp = l1.val + l2.val + carry
+    carry, value = tmp > 9 ? 1: 0, tmp > 9 ? tmp - 10 : tmp
+    result.next = ListNode.new(value)
+    result = result.next
+    l1, l2 = l1.next, l2.next
   end
-  remain = (l1==nil ? l2 : l1 )
-  while remain!=nil
-    (result.next=remain;break) if carry==0
-    sum = remain.val+carry
-    carry = sum/10
-    result.next = ListNode.new(sum%10)
-    result, remain = result.next, remain.next
-  end
-  result.next=ListNode.new(1) if carry!=0
+  result.next = carry == 0 ? l1 || l2 : add_two_numbers(l1 || l2, ListNode.new(1))
   dummy.next
 end
