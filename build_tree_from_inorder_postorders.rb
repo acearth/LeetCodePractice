5ac82a9a814800cfcf26d2942f78e071968eee30
Arthur <arthur4it@gmail.com>
2017-06-27 16:08:12 +0800

    Update build_tree_from_inorder_postorders.rb

--- build_tree_from_inorder_postorders.rb
+++ build_tree_from_inorder_postorders.rb
@@ -1,8 +1,8 @@
 def build_tree(inorder, postorder)
-  return nil if inorder.size==0
-  root=TreeNode.new(postorder[-1])
-  index=inorder.index(root.val)
-  root.left=build_tree(inorder[0...index], postorder[0...index])
-  root.right=build_tree(inorder[index+1..-1],postorder[index..-2])
-  root
-end
+    return if inorder.empty?
+    root = TreeNode.new(postorder[-1])
+    root_index = inorder.index(root.val)
+    root.left = build_tree(inorder[0...root_index], postorder[0...root_index])
+    root.right = build_tree(inorder[root_index + 1..-1], postorder[root_index..-2])
+    root
+en
