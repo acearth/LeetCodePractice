2ffc09f65ccf16318b6801ba4da041ee4cdfff3a
Arthur <arthur4it@gmail.com>
2017-06-30 19:46:21 +0800

    Update calculate_basic_string.rb

--- calculate_basic_string.rb
+++ calculate_basic_string.rb
@@ -1,37 +1,15 @@
+# s contains number, +, -, (), without * and /
 def calculate(s)
-  nums = [[]]
-  ops = [[]]
-  cur_num = -1
-  s.chars.each do |ch|
-    if ch.ord.between?(48, 57)
-      cur_num = 0 if cur_num == -1
-      cur_num = cur_num * 10 + ch.ord - 48 # ASCII code of '0' is 48
+  stack = []
+  s.scan(/\w+|\+|\-|\(|\)/).each do |token|
+    if token == '(' || token == '+' || token == '-'
+      stack << token.to_sym
     else
-      nums.last << cur_num if cur_num > -1
-      cur_num = -1
+      (token = stack.pop) && stack.pop if token == ')'
+      next stack << token.to_i unless stack.last == :+ || stack.last == :-
+      operator = stack.pop
+      stack[-1] = [stack[-1], token.to_i].inject(operator)
     end
-    process_current(ch, nums, ops)
   end
-  nums.last << cur_num if cur_num != -1
-  seq_eval(nums.pop, ops.pop)
+  stack.first
 end
-
-def process_current(ch, nums_stack, operator_stack)
-  if ch == '('
-    nums_stack << []
-    operator_stack << []
-  elsif ch == ')'
-    current = seq_eval(nums_stack.pop, operator_stack.pop)
-    nums_stack.last << current
-  elsif ch == '+' || ch == '-'
-    operator_stack.last << ch.to_sym
-  end
-end
-
-def seq_eval(nums, ops)
-  result = nums.first
-  ops.length.times { |i| result = [result, nums[i + 1]].reduce(ops[i]) }
-  result
-end
-
-# p calculate("(1+(4+5+2)-3)+(6+8)") == 23
