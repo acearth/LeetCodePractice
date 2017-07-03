5520496f166ffe87248d4b43521f00e752721bda
Arthur <arthur4it@gmail.com>
2017-07-03 14:24:47 +0800

    Update can_complete_circuit_Gas_Station.rb

--- can_complete_circuit_Gas_Station.rb
+++ can_complete_circuit_Gas_Station.rb
@@ -1,28 +1,10 @@
 def can_complete_circuit(gas, cost)
-  net,n=[],gas.size
-  n.times{|i| net[i]=gas[i]-cost[i]}
-  return -1 if net.inject(:+)<0
-  getStartPoint(net)
-end
-
-def getStartPoint(net)
-  n=net.size
-  tail=n-1
-  start,sum=0,0
-  i=0
-  while i<tail
-    sum+=net[i]
-    while sum<0
-      start=(start-1)%n
-      sum+=net[start]
-      tail-=1
-    end
-    i+=1
+  due = remain = start = 0
+  gas.size.times do |location|
+    remain += gas[location] - cost[location]
+    next unless remain < 0
+    due, remain = due + remain, 0
+    start = location + 1
   end
-  start
+  remain + due < 0 ? -1 : start
 end
-
-a=[-1,2,-1]
-p getStartPoint(a)
-
-
