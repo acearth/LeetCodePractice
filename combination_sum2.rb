15ddf01f12ef822257738a1289eebab006f40f04
Arthur Ace <arthur4it@gmail.com>
2022-05-23 18:20:41 +0800

    combination_sum2 solved with optimization

--- combination_sum2.rb
+++ combination_sum2.rb
@@ -1,28 +1,19 @@
+# @param {Integer[]} candidates
+# @param {Integer} target
+# @return {Integer[][]}
 def combination_sum2(candidates, target)
-  result=Array.new
-  dfs(candidates, Array.new, 0, target, result)
-  result.sort!
-  result.uniq #Should I use uniq methed or implement this method?
+  search([], target, 0, candidates.sort)
 end
 
-def dfs(nums, tmpNums, curSum, target, result)
-  for i in 0..nums.size-1
-    if target==curSum+nums[i]
-      tmpNumsNew=tmpNums.dup
-      tmpNumsNew.push nums[i]
-      result.push tmpNumsNew.sort
-    elsif target>curSum+nums[i]
-      tmpNumsNew=tmpNums.dup
-      tmpNumsNew.push nums[i]
-      break if i==nums.size-1
-      dfs(nums[i+1..-1], tmpNumsNew, curSum+nums[i], target, result)
-    end
+def search(comb, remain, cur, candidates)
+  if remain == 0
+    [comb]
+  elsif remain < 0
+    []
+  else
+    (cur...candidates.size).flat_map do |next_cur|
+      next [] if next_cur > cur && candidates[next_cur] == candidates[next_cur - 1] #important optimization
+      search(comb + [candidates[next_cur]], remain - candidates[next_cur], next_cur + 1, candidates)
+    end.select(&:any?)
   end
 end
-
-ar=2, 3, 6, 7
-p combination_sum ar, 7
-ar=[8, 7, 4, 3]
-p combination_sum ar, 11
-ar=10, 1, 2, 7, 6, 1, 5
-p combination_sum ar, 8
