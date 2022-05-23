ca83974dc4add4afc55d92332f12b4ed2c9cc28f
Arthur Ace <arthur4it@gmail.com>
2022-05-23 16:42:45 +0800

    combination_sum solved again

--- combination_sum.rb
+++ combination_sum.rb
@@ -1,27 +1,23 @@
 def combination_sum(candidates, target)
-  result=Array.new
-  dfs(candidates, Array.new, 0, target, result)
-  result.each {|re| re.sort!}
-  result.sort
+  search(0, [], target, candidates)
 end
 
-def dfs(nums, tmpNums, curSum, target, result)
-  if target==curSum
-    result.push tmpNums
-    return
+def search(cur_sum, cur_combination, target, candidates)
+  if cur_sum > target
+    []
+  elsif cur_sum == target
+    [cur_combination.sort]
   else
-    for i in 0..nums.size-1
-      if target>=curSum+nums[i]
-        tmpNumsNew=tmpNums.dup
-        tmpNumsNew.push nums[i]
-        dfs(nums[i..-1], tmpNumsNew, curSum+nums[i], target, result)
-      end
-    end
+    candidates.flat_map do |cur|
+      search(cur_sum + cur, [cur] + cur_combination, target, candidates)
+    end.select(&:any?).uniq
   end
 end
 
-ar=2, 3, 6, 7
-p combination_sum ar, 7
-ar=[8,7,4,3]
-p combination_sum ar, 11
-
+# ar = 2, 3, 6, 7
+# p combination_sum ar, 7
+# ar = [8, 7, 4, 3]
+# p combination_sum ar, 11
+#
+# ar = [2, 3, 5]
+# p combination_sum(ar, 8)
