e64c3ad9852b7463e86dd3bbcf95aa3baaa23cac
Arthur Ace <arthur4it@gmail.com>
2022-05-23 23:27:35 +0800

    add local playground for ruby

--- combine_n_k.rb
+++ combine_n_k.rb
@@ -1,22 +1,43 @@
-def combine(n, k)
-  endState =[]
-  (n).downto(n-k+1).each { |w| endState.unshift w }
-  start=[]
-  (1..k).each { |w| start.push w }
-  result=[start.dup]
-  while start!=endState
-    cur=k-1
-    while start[cur]==endState[cur]
-      cur-=1
-    end
-    start[cur]+=1
-    (cur+1..k-1).each{|i| start[i]=start[i-1]+1}
-    result.push start.dup
-    cur=k-1
+# @param {Integer} n
+# @param {Integer} k
+# @return {Integer[][]}
+def combine_2015(n, k)
+  head, tail = (1..k).to_a, (n - k + 1..n).to_a
+  result = [[] + head]
+  while head != tail
+    last = k - 1
+    last -= 1 while head[last] == tail[last]
+    head[last] += 1 #increase head
+    (last + 1...k).each { |i| head[i] = head[i - 1] + 1 } #reset combination
+    result << [] + head
   end
-  return result.size,result
+  result
 end
 
-p combine 4, 2
-p combine 5, 3
+def combine_dfs(n, k)
+  dfs(0, [], k, n)
+end
 
+def dfs(start, comb, k, n)
+  if comb.size == k
+    [comb]
+  else
+    (start + 1..n).flat_map do |cur|
+      dfs(cur, comb + [cur], k, n)
+    end
+  end
+end
+
+# @param {Integer} n
+# @param {Integer} k
+# @return {Integer[][]}
+def combine_pascal(n, k)
+  return [(1..k).to_a] if k == 0 || k == n
+  combine_pascal(n - 1, k - 1).map { |line| line + [n] } + combine_pascal(n - 1, k)
+end
+
+def combine(n,k)
+  combine_pascal(n,k)
+  # combine_2015(n,k)
+  # combine_dfs(n,k)
+end
