fa1349e754ffb00e346bd3d101fc0c45a2c47e42
Arthur Ace <arthur4it@gmail.com>
2022-06-02 17:18:07 +0800

    remove element review, check again

--- remove_element_From_array.rb
+++ remove_element_From_array.rb
@@ -1,10 +1,20 @@
+# Q-27: easy solution
 def remove_element(nums, val)
-  cleard=-1
-  nums.each_with_index do |n,i|
-    if val!=n
-      cleard+=1
-      nums[cleard]=n
-    end
+  cleared = -1
+  nums.each_with_index do |n, i|
+    nums[cleared += 1] = n unless nums[i] == val
   end
-  cleard+1
+  cleared + 1
+end
+
+# Better solution with less operations, cautions with pointers(border)!
+def remove_element_directly(nums, val)
+  p, q = 0, nums.size - 1
+  while p <= q
+    p += 1 while nums[p] != val && p < nums.size
+    q -= 1 while nums[q] == val
+    break if p > q || p == nums.size || q == -1
+    nums[p], nums[q] = nums[q], nums[p]
+  end
+  p
 end
