0a0042d1614ca501105b888ffddc654a0ff015c5
Arthur Ace <arthur4it@gmail.com>
2022-06-02 21:54:01 +0800

    q59: spiral matrix II

--- spiral_matrix_II.rb
+++ spiral_matrix_II.rb
@@ -1,32 +1,69 @@
+# created on 23/jul/2015
 def generate_matrix(n)
-  return [] if n==0
-  a=[[]]
-  k=n+1
-  (0..n-1).each{|i| a[0][i]=i+1}
-  (1..n-1).each {|i| a[i]=[];a[i][n-1]=k;k+=1}
-  (n-2).downto(0).each {|i| a[n-1][i]=k;k+=1}
-  si,ti,k,direction=n-1,0,k,3
-  while k<=n*n
-    si,ti,k,direction=fixLine(a,si,ti,k,direction)
+  return [] if n == 0
+  a = [[]]
+  k = n + 1
+  (0..n - 1).each { |i| a[0][i] = i + 1 }
+  (1..n - 1).each { |i| a[i] = []; a[i][n - 1] = k; k += 1 }
+  (n - 2).downto(0).each { |i| a[n - 1][i] = k; k += 1 }
+  si, ti, k, direction = n - 1, 0, k, 3
+  while k <= n * n
+    si, ti, k, direction = fixLine(a, si, ti, k, direction)
   end
   a
 end
 
-def fixLine(arr,si,ti,k,direction)
-  step=direction>1? -1:1
-  ti+=step if direction.even?
-  si+=step if direction.odd?
-  while arr[si][ti]==nil
-    arr[si][ti]=k
-    k+=1
-    ti+=step if direction.even?
-    si+=step if direction.odd?
+def fixLine(arr, si, ti, k, direction)
+  step = direction > 1 ? -1 : 1
+  ti += step if direction.even?
+  si += step if direction.odd?
+  while arr[si][ti] == nil
+    arr[si][ti] = k
+    k += 1
+    ti += step if direction.even?
+    si += step if direction.odd?
   end
-  ti-=step if direction.even?
-  si-=step if direction.odd?
-  direction+=1
-  direction=0 if direction==4
-  return si,ti,k,direction
+  ti -= step if direction.even?
+  si -= step if direction.odd?
+  direction += 1
+  direction = 0 if direction == 4
+  return si, ti, k, direction
 end
 
-
+# Q-59: heuristic solution on 2/june/2022
+# Since each sub-while is similar, can we create one uniform to deal with it?
+def generate_matrix_heuristic(n)
+  i, j, cur = 0, 0, 0
+  matrix = n.times.map { |_| [0] * n }
+  while cur < n * n
+    # H-increase
+    while matrix[i] && matrix[i][j] == 0
+      cur += 1
+      matrix[i][j] = cur
+      j += 1
+    end
+    i, j = i + 1, j - 1
+    # V-increase
+    while matrix[i] && matrix[i][j] == 0
+      cur += 1
+      matrix[i][j] = cur
+      i += 1
+    end
+    i, j = i - 1, j - 1
+    #H-decrease
+    while matrix[i] && matrix[i][j] == 0
+      cur += 1
+      matrix[i][j] = cur
+      j -= 1
+    end
+    i, j = i - 1, j + 1
+    #V-decrease
+    while matrix[i] && matrix[i][j] == 0
+      cur += 1
+      matrix[i][j] = cur
+      i -= 1
+    end
+    i, j = i + 1, j + 1
+  end
+  matrix
+end
