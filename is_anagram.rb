e470a66721807ec1b5a31e02ab828a338b7b96a4
Arthur Ace <arthur4it@gmail.com>
2022-06-03 21:52:45 +0800

    is_anagram O(n) way

--- is_anagram.rb
+++ is_anagram.rb
@@ -1,3 +1,15 @@
-def is_anagram(s,t)
-  s.chars.sort==t.chars.sort
+# O(n*n) solution of sorting
+def is_anagram_nn(s, t)
+  s.chars.sort == t.chars.sort
+end
+
+#O(n) solution of hash-like way
+def is_anagram(s, t)
+  return false unless s.size == t.size
+  sa, ta = [0] * 26, [0] * 26
+  s.size.times do |i|
+    sa[s[i].ord - 'a'.ord] += 1
+    ta[t[i].ord - 'a'.ord] += 1
+  end
+  sa == ta
 end
