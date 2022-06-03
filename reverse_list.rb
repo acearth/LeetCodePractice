689a2c5c52f7d2da922308d9c0e27b9b85982c03
Arthur Ace <arthur4it@gmail.com>
2022-06-03 14:34:07 +0800

    q-206: reverse list in iter and recur way

--- reverse_list.rb
+++ reverse_list.rb
@@ -1,4 +1,17 @@
+# q-206: reverse list iteratively and recursively
 def reverse_list(head)
+  got = nil
+  while head
+    next_node = head.next
+    head.next = got
+    got = head
+    head = next_node
+  end
+  got
+end
+
+# recursive way on 2016
+def reverse_list_recursively(head)
   return reverse(head).first if head
 end
 
