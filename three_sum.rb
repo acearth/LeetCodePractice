5588a183c4881e9df1c2593f2ec1291985c4bfc0
Arthur Ace <arthur4it@gmail.com>
2022-06-04 01:04:36 +0800

    q-15: 3-sum. Two pointer solution Important

--- three_sum.rb
+++ three_sum.rb
@@ -1,22 +1,25 @@
+#q-15: two pointer solution
 def three_sum(nums)
   nums.sort!
-  len, res = nums.length, []
-  len.times do |base|
-    break if nums[base] > 0
-    next if base > 0 && nums[base] == nums[base - 1]
-    left, right = base + 1, len - 1
+  result = []
+  nums.each_with_index do |n, i|
+    break if n > 0 # important speed up
+    left, right = i + 1, nums.size - 1
+    next if i > 0 && n == nums[i - 1] # note: speed up condition
     while left < right
-      if (sum = nums[left] + nums[right] + nums[base]) == 0
-        res << [nums[base], nums[left], nums[right]]
-        right -= 1 while nums[right] == nums[right - 1]
-        left += 1 while nums[left] == nums[left + 1]
-        left, right = left + 1, right - 1
-      elsif sum > 0
+      if nums[left] + nums[right] + n > 0
         right -= 1
-      else
+      elsif nums[left] + nums[right] + n < 0
         left += 1
+      else
+        result << [n, nums[left], nums[right]]
+        # Do remove-dup after least one tuple is found
+        # Otherwise right <= left then some answer been rejected
+        left += 1 while nums[left] == nums[left + 1]
+        right -= 1 while nums[right] == nums[right -1]
+        left, right = left + 1, right - 1
       end
     end
   end
-  res
+  result
 end
