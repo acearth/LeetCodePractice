5e1f79667dae87daad9939446b3324db7ceb1532
Arthur Ace <arthur4it@gmail.com>
2022-06-03 23:41:15 +0800

    q-1: better code with readible and debug-able feature

--- two_sum.rb
+++ two_sum.rb
@@ -1,3 +1,15 @@
-def two_sum(nums, target, have = {})
+# q-1: better solution for reading and debugging
+def two_sum(nums, target)
+  has = {}
+  nums.each_with_index do |n, i|
+    if has[target - n]
+      return [has[target - n], i]
+    else
+      has[n] = i
+    end
+  end
+end
+
+def two_sum_one_liner(nums, target, have = {})
   nums.each_with_index { |n, i| have[k = target - n] ? (return [have[k], i]) : have[n] = i }
 end
