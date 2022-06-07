7aa72e0aa2008aeb186febf4066b4343be78c1bb
Arthur Ace <arthur4it@gmail.com>
2022-06-07 19:03:32 +0800

    min depth of tree: + iterate way

--- min_depth_Tree.rb
+++ min_depth_Tree.rb
@@ -1,7 +1,18 @@
-def min_depth(root)
+def min_depth_recursive(root)
   return 0 if not root
   return min_depth(root.left)+1 if not root.right
   return min_depth(root.right)+1 if not root.left
   [min_depth(root.left), min_depth(root.right)].min + 1
 end
 
+def min_depth(root)
+  return 0 unless root
+  depth, q = 0, [root]
+  while q.any?
+    depth+=1
+    q.map! do |node|
+      return depth if !node.left && !node.right
+      [node.left,node.right].compact
+    end.flatten!
+  end
+end
