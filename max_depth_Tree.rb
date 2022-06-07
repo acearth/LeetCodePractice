7ae978c888e95c243858b8c41868d64106c9a2e4
Arthur Ace <arthur4it@gmail.com>
2022-06-07 18:50:49 +0800

    max depth of tree: +iter way

--- max_depth_Tree.rb
+++ max_depth_Tree.rb
@@ -1,4 +1,14 @@
-def max_depth(tree)
+def max_depth_recursive(tree)
   return 0 if not tree
   [max_depth(tree.left), max_depth(tree.right)].max + 1
+end
+
+def max_depth(root)
+  depth = 0
+  q = [root]
+  while q.any?
+    depth += 1
+    q.map! { |node| [node.left, node.right].compact }.flatten!
+  end
+  depth
 end
