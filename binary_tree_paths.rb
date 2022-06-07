e20fb58d2e366b1942701f50d1316f2161031785
Arthur Ace <arthur4it@gmail.com>
2022-06-07 22:43:36 +0800

    q-257: binary tree paths recur + iter

--- binary_tree_paths.rb
+++ binary_tree_paths.rb
@@ -1,4 +1,4 @@
-def binary_tree_paths(tree)
+def binary_tree_paths_recursive(tree)
   path(tree).map { |p| p.join("->") }
 end
 
@@ -8,3 +8,22 @@ def path(tree)
   (path(tree.left) + path(tree.right)).map { |p| [tree.val] + p }
 end
 
+# Q-257 with preorder traversal:
+# (1) companion- paths stack for collecting path, sync with stack
+# (2) note for output condition (leaf rather than nil)
+def binary_tree_paths(root)
+  paths, stack, result = [[root.val]], [root], []
+  while stack.any?
+    tree, path = stack.pop, paths.pop
+    result << path.join('->') if !tree.left && !tree.right
+    if tree.right
+      stack << tree.right
+      paths << path + [tree.right.val]
+    end
+    if tree.left
+      stack << tree.left
+      paths << path + [tree.left.val]
+    end
+  end
+  result
+end
