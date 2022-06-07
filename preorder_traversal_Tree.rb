a0757d9edf4b8fbd62f7234d15673063fa82bba7
Arthur Ace <arthur4it@gmail.com>
2022-06-07 16:03:01 +0800

    q-148: preorder tree recur+iter

--- preorder_traversal_Tree.rb
+++ preorder_traversal_Tree.rb
@@ -1,9 +1,15 @@
-def preorder_traversal(tree)
-  stack, result = [tree], []
-  while stack.any?
-    result << (tree = stack.pop).val
-    stack << tree.right if tree.right
-    stack << tree.left if tree.left
+# Q-148: preorder tree, recursive and iterative
+def preorder_traversal_recursive(root)
+  return [] unless root
+  [root.val] + preorder_traversal(root.left) + preorder_traversal(root.right)
+end
+
+def preorder_traversal(root)
+  stack, result = [root], []
+  while cur = stack.pop
+    result << cur.val
+    stack << cur.right if cur.right
+    stack << cur.left if cur.left
   end
   result
 end
