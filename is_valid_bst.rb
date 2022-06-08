3be28742c56e858779f0141212ec74105d482ca0
Arthur Ace <arthur4it@gmail.com>
2022-06-08 15:07:05 +0800

    Q-98: is valid BST? iter + recur

--- is_valid_bst.rb
+++ is_valid_bst.rb
@@ -1,10 +1,28 @@
-def is_valid_bst(root)
-  return true if root==nil
-  is_valid(root.left, (-1*2**32..root.val-1)) && is_valid(root.right,(root.val+1..2**32))
+# Q-98: is valid BST? iter + recur
+def is_valid_bst_recur(root)
+  is_valid(root, (-1 * 2 ** 32...2 ** 32))
 end
 
-def is_valid(tree, interval)
-  return true if tree==nil
-  return false if not interval.include? tree.val
-  is_valid(tree.left,(interval.begin..tree.val-1)) && is_valid(tree.right, (tree.val+1..interval.end))
+def is_valid(tree, range)
+  return true unless tree
+  return false unless range.include? tree.val
+  is_valid(tree.left, (range.begin...tree.val)) && is_valid(tree.right, (tree.val + 1...range.end))
+end
+
+def is_valid_bst(root)
+  stack, range = [root], [-1 * 2 ** 32...2 ** 32]
+  while stack.any?
+    tree = stack.pop
+    curr = range.pop
+    return false unless curr.include? tree.val
+    if tree.right
+      stack << tree.right
+      range << (tree.val + 1...curr.end)
+    end
+    if tree.left
+      stack << tree.left
+      range << (curr.begin...tree.val)
+    end
+  end
+  true
 end
