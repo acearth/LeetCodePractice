d3fc5ccc654677281539f1217d5c8a7b3ceaf4c0
Arthur Ace <arthur4it@gmail.com>
2022-06-08 23:24:30 +0800

    Q-108: sorted_array_to_bst_20Aug2015.rb recur+iter way, iter need retry

--- sorted_array_to_bst_20Aug2015.rb
+++ sorted_array_to_bst_20Aug2015.rb
@@ -1,16 +1,26 @@
-require './base/TreeNode'
-
+# Q-108: NOTE: iter-way easy to fail!
 def sorted_array_to_bst(nums)
-  return nil if nums.size==0
-  m=nums.size/2
-  arr=TreeNode.new(nums[m])
-  arr.left=sorted_array_to_bst(nums[0...m])
-  arr.right=sorted_array_to_bst(nums[m+1..-1])
-  return arr
+  return if nums.empty?
+  mid = nums.size / 2
+  TreeNode.new(nums[mid], sorted_array_to_bst(nums[0...mid]), sorted_array_to_bst(nums[mid + 1..]))
 end
-a=[1,2,3,4,5,6,7]
-a=[1,2,3,4,5,6]
-t=sorted_array_to_bst(a)
-t.preOrder
-puts
-t.midOrder
+
+# TODO-retry
+def sorted_array_to_bst(nums)
+  root = TreeNode.new(0)
+  ranges, q = [0...nums.size], [root]
+  while q.any?
+    cur, rg = q.shift, ranges.shift
+    mid = rg.begin + (rg.end - rg.begin) / 2
+    cur.val = nums[mid]
+    if rg.begin < mid
+      q << cur.left = TreeNode.new(0)
+      ranges << (rg.begin...mid)
+    end
+    if mid + 1 < rg.end
+      q << cur.right = TreeNode.new(0)
+      ranges << (mid + 1...rg.end)
+    end
+  end
+  root
+end
