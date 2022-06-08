55da165a66fe913b04ba69b973fd2721f4796a72
Arthur Ace <arthur4it@gmail.com>
2022-06-08 17:26:18 +0800

    Q-236: find ancester: hard recursive, retry it again!

--- lowest_common_ancestor_of_bst_21Aug.rb
+++ lowest_common_ancestor_of_bst_21Aug.rb
@@ -1,6 +1,8 @@
+# Q-236: post order traversal and find it
+# NOTE for border condition
 def lowest_common_ancestor(root, p, q)
-  sta=root.val<=>p.val
-  return root if sta!=(root.val<=>q.val)
-  retur lowest_common_ancestor(root.left,p,q) if sta>0
-  lowest_common_ancestor(root.right,p,q)
+  return root if [p, q, nil].include? root
+  left = lowest_common_ancestor(root.left, p, q)
+  right = lowest_common_ancestor(root.right, p, q)
+  left && right ? root : left || right
 end
