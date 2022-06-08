adef16bd6a5a8f0a1837b719ba3f0eb1815dee5f
Arthur Ace <arthur4it@gmail.com>
2022-06-08 12:49:31 +0800

    tree paths in iter, recur, back-tracking ways

--- binary_tree_paths.rb
+++ binary_tree_paths.rb
@@ -12,10 +12,10 @@ end
 # (1) companion- paths stack for collecting path, sync with stack
 # (2) note for output condition (leaf rather than nil)
 def binary_tree_paths(root)
-  paths, stack, result = [[root.val]], [root], []
+  return [] unless root
+  stack, paths, result = [root], [[root.val]], []
   while stack.any?
     tree, path = stack.pop, paths.pop
-    result << path.join('->') if !tree.left && !tree.right
     if tree.right
       stack << tree.right
       paths << path + [tree.right.val]
@@ -24,6 +24,37 @@ def binary_tree_paths(root)
       stack << tree.left
       paths << path + [tree.left.val]
     end
+    result << path.join('->') unless tree.left || tree.right
   end
   result
+end
+
+def binary_tree_paths_backtracking(root)
+  result = []
+  back(root, [root.val], result)
+  result
+end
+
+def back(root, path, result)
+  result << path.join('->') unless root.left || root.right
+  if root.left
+    path << root.left.val
+    back(root.left, path, result)
+    path.pop
+  end
+  if root.right
+    path << root.right.val
+    back(root.right, path, result)
+    path.pop
+  end
+end
+
+def binary_tree_paths_recur(tree)
+  path(tree).map { |p| p.join("->") }
+end
+
+def path(tree)
+  return [] unless tree
+  return [[tree.val]] if !tree.left && !tree.right
+  (path(tree.left) + path(tree.right)).map { |p| [tree.val] + p }
 end
