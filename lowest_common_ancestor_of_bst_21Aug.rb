fc78ce41c83df73501f7e2f8d93040cfd396d674
Arthur Ace <arthur4it@gmail.com>
2022-06-08 17:47:33 +0800

    q-235: ancestor of BST(rather than common tree!) recur+iter

--- lowest_common_ancestor_of_bst_21Aug.rb
+++
@@ -1,9 +0,0 @@
-# Q-235: of BST rather than common tree, check Q-236 for comparing.
-def lowest_common_ancestor(root, p, q)
-  if (root.val <=> p.val) == (root.val <=> q.val)
-    next_node = root.val > p.val ? root.left : root.right
-    lowest_common_ancestor(next_node, p, q)
-  else
-    root
-  end
-end
