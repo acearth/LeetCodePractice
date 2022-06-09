ca7c919fc67da6f7983ff8eba54733f14579c45b
Arthur Ace <arthur4it@gmail.com>
2022-06-09 15:06:38 +0800

    Q-131: partition palindrome by backtracking

--- partition_palindrome.rb
+++ partition_palindrome.rb
@@ -1,27 +1,17 @@
+# q-131: partition palindrome by backtracking
 def partition(s)
-  result=[[s[0]]]
-  s[1..-1].chars.each do |ch|
-    result.each do |part|
-      part<<ch
-      tmp1=part[-1]+ch
-      if tmp1.reverse==tmp1
-        dupPart=part.dup
-        dupPart[-1]=tmp1
-        result<<dupPart
-      end
-      if part.size>1
-        tmp2=part[-2]+tmp1
-        if tmp2.reverse==tmp2
-          dupPart=part[0..-2].dup
-          dupPart<<tmp2
-          result<<dupPart
-        end
-      end
-    end
-  end
+  result =[]
+  backtracking(s,0,[], result)
   result
 end
-s='aab'
-p partition(s)
-p partition('efe')
 
+def backtracking(str, start,collected, result)
+  return result << [] + collected if start == str.size
+  (start...str.size).each do |i|
+    sub = str[start..i]
+    next unless sub == sub.reverse
+    collected << sub
+    backtracking(str,i+1, collected, result)
+    collected.pop
+  end
+end
