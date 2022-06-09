15d06b050d655c9a699d40b2fa3a6630aa997b6a
Arthur Ace <arthur4it@gmail.com>
2022-06-09 13:55:48 +0800

    Q17-backtracking: phone numbers

--- letter_combinations_digits.rb
+++ letter_combinations_digits.rb
@@ -1,17 +1,24 @@
+# Q-17 backtracking
 def letter_combinations(digits)
-  set=%w[abc def ghi jkl mno pqrs tuv wxyz]
-  hash={}
-  set.each_with_index { |chs, i| hash[(i+2).to_s]=chs }
-  return [] if digits.length<1
-  letter_combination_helper(hash, digits)
+  keys = %w[abc def ghi jkl mno pqrs tuv wxyz].map(&:chars)
+  result = []
+  backtracking0(digits.chars.map(&:to_i).map { |ch| keys[ch - 2] }, 0, '', result)
+  result.reject(&:empty?)
 end
 
-def letter_combination_helper(hash, digits)
-  return hash[digits[0]].chars if digits.length==1
-  pre=letter_combination_helper(hash, digits[0...-1])
-  res=[]
-  hash[digits[-1]].chars.each do |ch|
-    pre.each { |st| res<<st+ch }
+def backtracking(keys, step, collected, result)
+  return result << collected.join if step == keys.size
+  keys[step].each do |ch|
+    collected << ch
+    backtracking(keys, step + 1, collected, result)
+    collected.pop
   end
-  res
+end
+
+# Faster but less typical
+def backtracking0(keys, step, collected, result)
+  return result << collected if step == keys.size
+  keys[step].each do |ch|
+    backtracking0(keys, step + 1, collected + ch, result)
+  end
 end
