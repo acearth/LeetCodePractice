765d399e7b2ce489a7b6fb78ea80c429db5355f9
Arthur Ace <arthur4it@gmail.com>
2022-06-09 17:52:10 +0800

    Q-46: permute +backtracking

--- permute_distinct.rb
+++ permute_distinct.rb
@@ -1,5 +1,25 @@
-def permute(nums)
-  return      [nums] if nums.size < 2
-      permute(nums[0...-1]).flat_map { |line| 0.upto(line.size).map { |i| line.dup.insert(i, nums[-1]) } }
-end
 # Acknowledgement: Thanks to Chaoyang Che for improving my solution.
+def permute_trick(nums)
+  return [nums] if nums.size < 2
+  permute(nums[0...-1]).flat_map { |line| 0.upto(line.size).map { |i| line.dup.insert(i, nums[-1]) } }
+end
+
+# Q-46: @note: use visited hash. Reset after tracked.
+def permute(nums)
+  backtracking(nums, [], nums.zip([false] * nums.size).to_h, result = [])
+  result
+end
+
+def backtracking(nums, got, visited, result)
+  return result << [] + got if got.size == nums.size
+  nums.each do |n|
+    next if visited[n]
+    got << n
+    visited[n] = true
+    backtracking(nums, got, visited, result)
+    got.pop
+    visited[n] = false
+  end
+end
+
+p permute([1, 2, 3])
