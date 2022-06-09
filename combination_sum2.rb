926a1459a2a810040a641edfdd75ed30cfb3b444
Arthur Ace <arthur4it@gmail.com>
2022-06-09 14:43:44 +0800

    Q-40: combination_sum2.rb: (1) remove dup; (2) prune ASAP otherwise result in wrong answer

--- combination_sum2.rb
+++ combination_sum2.rb
@@ -1,19 +1,18 @@
-# @param {Integer[]} candidates
-# @param {Integer} target
-# @return {Integer[][]}
 def combination_sum2(candidates, target)
-  search([], target, 0, candidates.sort)
+  result = []
+  backtracking(candidates.sort, 0, [], target, result)
+  result.uniq
 end
 
-def search(comb, remain, cur, candidates)
-  if remain == 0
-    [comb]
-  elsif remain < 0
-    []
-  else
-    (cur...candidates.size).flat_map do |next_cur|
-      next [] if next_cur > cur && candidates[next_cur] == candidates[next_cur - 1] #important optimization
-      search(comb + [candidates[next_cur]], remain - candidates[next_cur], next_cur + 1, candidates)
-    end.select(&:any?)
+# @note: (1) remove duplicates; (2) prune ASAP, fails if prune on return condition
+def backtracking(candidates, start, path, target, result)
+  return result << [] + path if target == 0 #@note: diff here
+  (start...candidates.size).each do |i|
+    # note: slide by when i > start, rather than i > 0, since we need to enter at least once!
+    next if i > start && candidates[i] == candidates[i - 1] && candidates[i - 1] == candidates[i - 2]
+    break if target - candidates[i] < 0 # @note: eagerly pruned;  diff here
+    path << candidates[i]
+    backtracking(candidates, i + 1, path, target - candidates[i], result)
+    path.pop
   end
 end
