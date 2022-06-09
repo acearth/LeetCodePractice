a0594c13e133d41c2c5c387cc17c753141ba950a
Arthur Ace <arthur4it@gmail.com>
2022-06-09 18:52:43 +0800

    Q-52: N-queens, formed backtracking solution

--- total_n_queens.rb
+++ total_n_queens.rb
@@ -1,26 +1,26 @@
-# @param lines: a 1-d array lines[0] stands for line0's location in column
-# @param i: current line number
-def search(lines, i, result)
-  return @result += 1 if i == lines.size
-  lines.size.times do |j|
-    lines[i] = j
-    if placeable(lines, i, j)
-      search(lines, i + 1, result)
-    end
-    lines[i] = nil
+# Q-52: N-queens: formed backtracking solution
+def total_n_queens(n)
+  backtracking(n, 0, [], @result = 0)
+  @result
+end
+
+def backtracking(n, level, placed, result)
+  return @result += 1 if level == n
+
+  n.times.each do |j|
+    next unless placeable?(placed, level, j)
+
+    placed << j
+    backtracking(n, level + 1, placed, result)
+    placed.pop
   end
 end
 
-# @param j: current column number
-def placeable(lines, i, j)
-  i.times { |k| return false if [j - k + i, j, j + k - i].include?(lines[k]) }
+# @note: Given place is (i,j), how about the 0-th line? it shall be (0,j-i)
+#        The 1-st line shall be (1,j-i+1), the X-th line shall be (j-i+X).
+def placeable?(placed, i, j)
+  i.times do |cur|
+    return false if placed[cur] == j - cur + i || placed[cur] == j + cur - i || placed[cur] == j
+  end
   true
-end
-
-# @note: have to use instance variable "@result" or [0], otherwise the result always be initial value
-# @note: we maybe not pass the plain number as reference value??? TODO-confirm
-def total_n_queens(n)
-  location, @result = [nil] * n, 0
-  search(location, 0, @result)
-  @result
 end
