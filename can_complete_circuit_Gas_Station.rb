25aa8f51e87aeee8aedae13321cdfed3cbbc5271
Arthur Ace <arthur4it@gmail.com>
2022-06-11 14:54:28 +0800

    Q-134: gas station curcuit: greeding

--- can_complete_circuit_Gas_Station.rb
+++ can_complete_circuit_Gas_Station.rb
@@ -1,3 +1,4 @@
+# Q-134: solution before 2018
 def can_complete_circuit(gas, cost)
   due = remain = start = 0
   gas.size.times do |location|
@@ -7,4 +8,22 @@ def can_complete_circuit(gas, cost)
     start = location + 1
   end
   remain + due < 0 ? -1 : start
+end
+
+
+# solution on 2022
+def can_complete_circuit(gas, cost)
+  p net = gas.zip(cost).map { |i| i.reduce(:-) }
+  remain = 0
+  location = 0
+  net.each_with_index do |have, i|
+    if remain + have < 0
+      location = i + 1
+      remain = 0
+    else
+      remain += have
+    end
+  end
+  return -1 if location == net.size || net.sum < 0
+  location
 end
