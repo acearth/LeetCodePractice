f394dbe5aed783626ffbd8debc25fc8b8ac3c599
Arthur Ace <arthur4it@gmail.com>
2022-06-11 21:26:29 +0800

    Q-63: unique_paths_with_obstacles: DP easy

--- unique_path_with_obstacles.rb
+++ unique_path_with_obstacles.rb
@@ -1,30 +1,18 @@
-# @param {Integer[][]} obstacle_grid
-# @return {Integer}
-def unique_paths_with_obstacles(obstacle_grid)
-  p=[]
-  obstacle_grid.size.times {p.push Array.new(obstacle_grid[0].size,0)}
-  obstacle_grid.size.times do |i|
-    obstacle_grid[0].size.times do |j|
-      return 0 if i==0&&j==0 && obstacle_grid[i][j]==1
-      p[i][j]=1 if i==0&& j==0
-      if i==0&&j>0
-        p[i][j]=p[i][j-1] if obstacle_grid[i][j]==0
-        p[i][j]=0 if obstacle_grid[i][j]==1
-      elsif i>0&&j==0
-        p[i][j]=p[i-1][j] if obstacle_grid[i][j]==0
-        p[i][j]=0 if obstacle_grid[i][j]==1
-      else
-        p[i][j]=p[i-1][j]+p[i][j-1]
-        p[i][j]=0 if obstacle_grid[i][j]==1
-      end
+# Q-63: unique_paths_with_obstacles: DP easy
+def unique_paths_with_obstacles(grid)
+  m, n = grid.size, grid.first.size
+  dp = m.times.map { |_| [0] * n }
+  dp[0][0] = 1 - grid[0][0]
+  (1...m).each do |i|
+    dp[i][0] = grid[i][0] == 1 ? 0 : dp[i - 1][0]
+  end
+  (1...n).each do |i|
+    dp[0][i] = grid[0][i] == 1 ? 0 : dp[0][i - 1]
+  end
+  (1...m).each do |i|
+    (1...n).each do |j|
+      dp[i][j] = grid[i][j] == 1 ? 0 : dp[i][j - 1] + dp[i - 1][j]
     end
   end
-  p
+  dp[-1][-1]
 end
-a=[
-  [0,0,0],
-  [0,1,0],
-  [0,0,0]
-]
-p unique_paths_with_obstacles(a)
-
