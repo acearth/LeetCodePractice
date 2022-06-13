7f1bdbdb03ade9145da85181034a7bfe7cff0777
Arthur Ace <arthur4it@gmail.com>
2022-06-14 00:24:46 +0800

    Q-674 easy DP solution for continuous increasing subseq..

--- word_break_dfsin.rb
+++ word_break_dfsin.rb
@@ -1,9 +1,13 @@
-def word_break(s, dict)
-  return true if s == ''
-  dict.each do |word|
-    next unless word == s[0...word.size]
-    return true if word_break(s[word.size..-1], dict)
-    dict.delete(word)
+def word_break(s, word_dict)
+  dp = [true] + [false] * s.size
+  (1..s.size).each do |i|
+    0.upto(i) do |j|
+      next if !word_dict.include? s[j...i]
+      dp[i] = dp[j]
+    end
   end
-  false
+  dp[s.size]
 end
+
+p word_break('leetcode', ['leet', 'code'])
+p word_break('applepenapple', ['apple', 'pen'])
