18a3d859f6daeb42cf6cd66dcba7d57359a3ab87
Arthur Ace <arthur4it@gmail.com>
2022-06-14 19:29:49 +0800

    Q-121 best time to buy stock I: dp way

--- max_profit_I.rb
+++ max_profit_I.rb
@@ -10,3 +10,16 @@ def max_profit(prices)
   end
   profit
 end
+
+# @note DP
+# state-0: Don't buy and hold the cash, count the cash's amount. Use 0 - prices[i] since we don't know the balance
+# state-1: After potential sell, how many profix can be gained?
+def max_profit_dp(prices)
+  dp = prices.size.times.map { [0, 0] }
+  dp[0] = [-prices[0], 0]
+  (1...prices.size).each do |i|
+    dp[i][0] = [dp[i - 1][0], -prices[i]].max
+    dp[i][1] = [dp[i - 1][1], prices[i] + dp[i - 1][0]].max
+  end
+  dp[-1][-1]
+end
