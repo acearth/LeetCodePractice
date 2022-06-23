071806c05fd4d58e749b6c8b8cb49ea5865c5d3f
Arthur Ace <arthur4it@gmail.com>
2022-06-23 12:48:30 +0800

    Q-402: monotonic stack

--- remove_kdigits.rb
+++ remove_kdigits.rb
@@ -1,16 +1,14 @@
-# question-402
+# Q-402
 def remove_kdigits(num, k)
-  integers = num.chars.map(&:to_i)
+  num = num.chars.map(&:to_i)
   stack = []
-  integers.each do |cur|
-    # create one stack to keep accepted digits
-    # remove larger digits than current digit
-    while k > 0 && stack.any? && stack.last > cur
+  num.each do |n|
+    while stack.any? && stack.last > n && k > 0
       stack.pop
       k -= 1
     end
-    stack << cur
+    stack << n
   end
-  k.times { stack.pop } # tail is larger than head, then remove it when we have to
-  stack.join.to_i.to_s # remove leading zeroes
+  k.times { stack.pop } # don't forget!
+  stack.join.to_i.to_s
 end
