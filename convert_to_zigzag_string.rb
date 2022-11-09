1617265da0c9fabee8aa2da02e0b2ee335bbe4df
Arthur Ace <arthur4it@gmail.com>
2022-11-09 15:28:53 +0800

    Q-6: zigzag by easy diff move

--- convert_to_zigzag_string.rb
+++ convert_to_zigzag_string.rb
@@ -1,3 +1,4 @@
+# Q-6
 def convert(s, num_rows)
   return s if num_rows == 1
   group_size = num_rows * 2 - 2
@@ -10,4 +11,25 @@ def convert(s, num_rows)
     1.upto(num_rows - 2) { |i| res[i] += (group[i] || '') + (group[group_size - i] || '') }
   end
   res.join
+end
+
+
+# 09/nov/2022
+def convert_neo(s, num_rows)
+    return s if num_rows==1
+    result = num_rows.times.map{[]}
+    dv=[1,0]
+    dd=[-1,1]
+    h,v = 0, 0
+    diff=nil
+    s.chars.each_with_index do |ch, i|
+        result[h][v]=ch
+        if h==0
+            diff=dv
+        elsif h==num_rows - 1
+            diff=dd
+        end
+        h, v = (diff.first + h)%num_rows, diff.last + v
+    end
+    result.join
 end
