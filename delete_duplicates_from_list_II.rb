35b2aa4edc2142d0af8f17996c94d2a92d5737ce
Arthur Ace <arthur4it@gmail.com>
2022-11-11 18:58:00 +0800

    Q-82: medium and funny question, good for practise

--- delete_duplicates_from_list_II.rb
+++ delete_duplicates_from_list_II.rb
@@ -1,11 +1,45 @@
+# Q-82: funny
 def delete_duplicates(head)
   return head if head==nil ||head.next==nil
-  if head.val !=head.next.val
+  if head.value !=head.next.value
     head.next=delete_duplicates(head.next)
     head
   else
-    cur=head.val
-    head=head.next while head!=nil && head.val==cur
+    cur=head.value
+    head=head.next while head!=nil && head.value==cur
     delete_duplicates(head)
   end
+end
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+    dummy=ListNode.new(0, head)
+    pre=dummy
+    p = head
+    while p
+        dup=false
+        while p.next && p.val==p.next.val
+            dup=true
+            p=p.next
+        end
+        if dup
+            p=p.next
+        else
+            pre.next=p
+            pre=p
+            p=p.next
+        end
+    end
+    pre.next=p
+    dummy.next
+    
 end
