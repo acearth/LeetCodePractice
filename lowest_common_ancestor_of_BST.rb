7681d5123c74da0ab0a72283da30a52f5c9181d0
Arthur Ace <arthur4it@gmail.com>
2022-11-13 09:17:46 +0800

    Q-235: retreat

--- lowest_common_ancestor_of_BST.rb
+++ lowest_common_ancestor_of_BST.rb
@@ -1,7 +1,7 @@
 # Q-235: of BST rather than common tree, check Q-236 for comparing.
 def lowest_common_ancestor_recursive(root, p, q)
-  if (root.val <=> p.val) == (root.val <=> q.val)
-    next_node = root.val > p.val ? root.left : root.right
+  if (root.value <=> p.value) == (root.value <=> q.value)
+    next_node = root.value > p.value ? root.left : root.right
     lowest_common_ancestor(next_node, p, q)
   else
     root
@@ -14,8 +14,15 @@ def lowest_common_ancestor(root, p, q)
   while stack.any?
     root = stack.pop
     return root if [p, q].include? root
-    return root if (root.val <=> p.val) != (root.val <=> q.val)
+    return root if (root.value <=> p.value) != (root.value <=> q.value)
     stack << root.right if root.right
     stack << root.left if root.left
   end
+end
+
+# Q-235: 13/nov/2022
+def lowest_common_ancestor(root, p, q)
+    return root if !root || root==p || root==q
+    return root if  root.val > p.val && root.val < q.val || root.val < p.val && root.val > q.val
+    lowest_common_ancestor(root.left, p,q) || lowest_common_ancestor(root.right,p ,q)    
 end
