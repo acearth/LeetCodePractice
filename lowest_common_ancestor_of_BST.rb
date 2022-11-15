42ae836cd35b370fc4804684f078fcb271637991
Arthur Ace <arthur4it@gmail.com>
2022-11-15 11:04:32 +0800

    refactor: remove wrong naming value -> val

--- lowest_common_ancestor_of_BST.rb
+++ lowest_common_ancestor_of_BST.rb
@@ -1,7 +1,7 @@
 # Q-235: of BST rather than common tree, check Q-236 for comparing.
 def lowest_common_ancestor_recursive(root, p, q)
-  if (root.value <=> p.value) == (root.value <=> q.value)
-    next_node = root.value > p.value ? root.left : root.right
+  if (root.val <=> p.val) == (root.val <=> q.val)
+    next_node = root.val > p.val ? root.left : root.right
     lowest_common_ancestor(next_node, p, q)
   else
     root
@@ -14,7 +14,7 @@ def lowest_common_ancestor(root, p, q)
   while stack.any?
     root = stack.pop
     return root if [p, q].include? root
-    return root if (root.value <=> p.value) != (root.value <=> q.value)
+    return root if (root.val <=> p.val) != (root.val <=> q.val)
     stack << root.right if root.right
     stack << root.left if root.left
   end
