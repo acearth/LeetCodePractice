301b8b836da9e262fde8084250c2e72203823d88
Arthur Ace <arthur4it@gmail.com>
2022-11-15 11:32:58 +0800

    move definiton of list and tree to source root

---
+++ list_node.rb
@@ -0,0 +1,37 @@
+# USAGE
+# 1. generate list by array
+# the_list = ListNode.generate([1,2,3])
+# 2. traverse
+# the_list.traverse   # => 1 -> 2 -> 3
+# ListNode.generate(['a','b','c']).traverse  # => a -> b -> c
+
+class ListNode
+  attr_accessor :val, :next
+
+  class << self
+    def generate(array)
+      array.map! { |v| ListNode.new(v) }
+      array.length.times { |i| array[i].next = array[i + 1] }
+      array.first
+    end
+  end
+
+  def initialize(value, next_node = nil)
+    @val = value
+    @next = next_node
+  end
+
+  def to_s
+    "#{@val}"
+  end
+
+  def traverse
+    print @val
+    print ' -> ' if self.next
+    self.next.traverse if self.next
+  end
+
+  def to_a
+    [@val] + self.next.to_a
+  end
+end
