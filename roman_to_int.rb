a57008041d911112555ea4b1f0dfc07ad0b1819e
Arthur Ace <arthur4it@gmail.com>
2022-11-17 18:16:31 +0800

    Q-13: roman to int: ideal solution

--- roman_to_int.rb
+++ roman_to_int.rb
@@ -1,10 +1,16 @@
-VALUE=[['I', 1], ['V', 5], ['X', 10], ['L', 50],
-       ['C', 100], ['D', 500], ['M', 1000]].to_h
-def roman_to_int(str)
-  return 0 if str==""
-  %w[M D C L X V I].each do |ch|
-    next if !str.include? ch
-    ci = str.index(ch)
-    return VALUE[ch] + roman_to_int(str[ci+1..-1]) - roman_to_int(str[0...ci])
+# Q-13:  Roman: Only one digits can be less than the principal and at most three less ones.
+def roman_to_int(s)
+  value = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+  result = 0
+  i = 0
+  while i < s.size
+    if i + 1 < s.size && value[s[i].to_sym] < value[s[i + 1].to_sym]
+      result += value[s[i + 1].to_sym] - value[s[i].to_sym]
+      i += 2
+    else
+      result += value[s[i].to_sym]
+      i += 1
+    end
   end
+  result
 end
