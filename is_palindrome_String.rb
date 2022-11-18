ac26364ad640127432293d2ec1c7b617d1eb1af7
Arthur Ace <arthur4it@gmail.com>
2022-11-18 13:11:59 +0800

    Q-125

--- is_palindrome_String.rb
+++ is_palindrome_String.rb
@@ -1,4 +1,15 @@
+# Q-125
+# @param {String} s
+# @return {Boolean}
 def is_palindrome(s)
-  s.gsub!(/\W/,'')
-  s.downcase==s.reverse.downcase
+  left, right = 0, s.size
+  while left < right
+    left += 1 while left < right && !(s[left] =~ /[A-Za-z0-9]/)
+    right -= 1 while left < right && !(s[right] =~ /[A-Za-z0-9]/)
+    if left < right
+      return false if s[left].upcase != s[right].upcase
+    end
+    left, right = left + 1, right - 1
+  end
+  true
 end
