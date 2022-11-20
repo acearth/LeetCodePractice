b7f18300bb4445bf254be0ef148a0304aec52df2
Arthur Ace <arthur4it@gmail.com>
2022-11-20 12:29:00 +0800

    Q-81: binary search with degeneration cases

--- search_rotated_array_with_duplicates_Target.rb
+++ search_rotated_array_with_duplicates_Target.rb
@@ -10,3 +10,33 @@ def search(nums, target)
   return search(nums[0..m-1], target) if nums[m] > target
   return search(nums[m+1..-1], target) if nums[m] < target
 end
+
+# Q-81: Worst case: nums[left] == nums[mid] == nums[right], then time complexity is O(n)
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Boolean}
+def search(nums, target)
+  search_range(nums, 0, nums.size - 1, target)
+end
+
+def search_range(nums, left, right, target)
+  return false if left > right
+  return nums[left] == target if left == right
+  m = (left + right) / 2
+  return true if [nums[left], nums[m], nums[right]].include? target
+  if nums[left] < nums[m]
+    if nums[m] > target && target > nums[left]
+      return search_range(nums, left + 1, m - 1, target)
+    else
+      return search_range(nums, m + 1, right - 1, target)
+    end
+  elsif nums[m] < nums[right]
+    if nums[m] < target && target < nums[right]
+      return search_range(nums, m + 1, right - 1, target)
+    else
+      return search_range(nums, left + 1, m - 1, target)
+    end
+  else
+    return search_range(nums, left + 1, m - 1, target) || search_range(nums, m + 1, right - 1, target)
+  end
+end
