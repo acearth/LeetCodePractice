0716d1080c6f3bd7d633e8cbfb74790a0da154cc
Arthur Ace <arthur4it@gmail.com>
2022-11-20 10:42:07 +0800

    Q-33: binary search with right inclusive and cases discussion

--- search_in_rotated_array.rb
+++ search_in_rotated_array.rb
@@ -1,22 +1,25 @@
+# Q-33: binary search (left and right inclusive)
+# @param {Integer[]} nums # rotated and sorted
+# @param {Integer} target
+# @return {Integer}
 def search(nums, target)
-  rotate_search(nums, 0, nums.length, target)
-end
-
-def rotate_search(nums, l, r, target)
-  return -1 if l == r
-  return nums[l] == target ? l : -1 if l + 1 == r
-  m = (l + r) / 2
-  return m if target == nums[m]
-  return rotate_search(nums, l, m, target) if m + 1 == r
-  normal, rotated = nums[m] > nums[l] ? [[l, m], [m+1, r]] : [[m+1, r], [l, m]]
-  unless target.between?(nums[normal.first], nums[normal.last - 1])
-    return rotate_search(nums, rotated.first, rotated.last, target)
-  end
-  l, r = normal.first, normal.last
-  while l < r
-    m = (l + r) / 2
+  left, right = 0, nums.size - 1
+  while left <= right
+    m = (left + right) / 2
     return m if nums[m] == target
-    target > nums[m] ? l = m + 1 : r = m
+    if nums[left] <= nums[m]
+      if nums[m] > target && target >= nums[left]
+        right = m - 1
+      else
+        left = m + 1
+      end
+    else
+      if target > nums[m] && target <= nums[right]
+        left = m + 1
+      else
+        right = m - 1
+      end
+    end
   end
   -1
 end
