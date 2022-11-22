28f2d66d1a5e44c7b9e4d6fdcebcdcd89c5194d8
Arthur Ace <arthur4it@gmail.com>
2022-11-22 22:28:04 +0800

    Q-269: topological sort solved originally!

--- alien_order_Q269-topological_sort.rb
+++ alien_order_Q269-topological_sort.rb
@@ -3,42 +3,37 @@
 # @param {String[]} words
 # @return {String}
 def alien_order(words)
-  pre = Hash.new { |h, k| h[k] = [] }
+  order = Hash.new { |h, k| h[k] = [] }
   words.size.times do |i|
     (i + 1...words.size).each do |j|
       first, second = compare(words[i], words[j])
-      return "" if !first || pre[second].include?(first)
-      pre[first] << second if first != second
+      return "" if !first || order[second].include?(first)
+      order[first] << second if first != second
     end
   end
   degrees = {}
   words.join.chars.each { |ch| degrees[ch] = 0 }
-  pre.keys.each do |k|
-    degrees[k] = 0 if !degrees.has_key?(k)
-    pre[k].each do |v|
-      degrees[v] += 1
-    end
+  order.keys.each do |from|
+    degrees[from] = 0 if !degrees.has_key?(from)
+    order[from].each { |to| degrees[to] += 1 }
   end
   result = []
   while degrees.any?
-    cur, _ = degrees.find { |_, d| d == 0 }
-    return "" if cur == nil
-    degrees.delete(cur)
-    result << cur
-    pre[cur].each do |v|
-      degrees[v] -= 1
-    end
+    head, _ = degrees.find { |_, degree| degree == 0 }
+    return "" unless head
+    degrees.delete(head)
+    result << head
+    order[head].each { |v| degrees[v] -= 1 }
   end
   result.join
 end
 
 def compare(word1, word2)
   len = [word1.size, word2.size].min
-  len.times do |i|
-    return [word1[i], word2[i]] if word1[i] != word2[i]
-  end
-  return [] if word1.size > word2.size # abc > ab <-- illegal case
-  word2[len] == nil ? [word1[-1], word1[-1]] : ['', word2[len]]
+  len.times { |i| return [word1[i], word2[i]] if word1[i] != word2[i] }
+  return [] if word1.size > word2.size # handle illegal case: "abc" > "ab"
+  return ['', ''] if word1.size == word2.size
+  ['', word2[len]] # when word2 longer than word1
 end
 
 p alien_order(["wrt", "wrf", "er", "ett", "rftt"]) == 'wertf'
