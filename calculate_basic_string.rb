7288d7792110cff4249c56aec9ba42a7581dc366
Arthur Ace <arthur4it@gmail.com>
2022-12-25 22:22:39 +0800

    Q-224: calculator I: basic with bracket

--- calculate_basic_string.rb
+++ calculate_basic_string.rb
@@ -1,15 +1,30 @@
-# s contains number, +, -, (), without * and /
+# Q-224: calculator I: s contains number, +, -, (), without * and /
+# 25/dec/2022
+# (1) treat "A - X" and "-X" as "A + (-X)" and "0 + (-X)";
+# (2) each bracket will always reduced to one number!
 def calculate(s)
+  result = 0
   stack = []
+  op = 0
+  sign = 1
   s.scan(/\w+|\+|\-|\(|\)/).each do |token|
-    if token == '(' || token == '+' || token == '-'
-      stack << token.to_sym
+    if token =~ /\d/
+      op = token.to_i
+    elsif token == '+' || token == '-'
+      result += sign * op
+      sign = token == '+' ? 1 : -1
+      op = 0
+    elsif token == '('
+      stack << result
+      stack << sign
+      result = 0
+      sign = 1
     else
-      (token = stack.pop) && stack.pop if token == ')'
-      next stack << token.to_i unless stack.last == :+ || stack.last == :-
-      operator = stack.pop
-      stack[-1] = [stack[-1], token.to_i].inject(operator)
+      result += sign * op
+      result *= stack.pop
+      result += stack.pop
+      op = 0
     end
   end
-  stack.first
+  result + op * sign
 end
