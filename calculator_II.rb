683e75d51f08b08d386cafad684256beff83f937
Arthur Ace <arthur4it@gmail.com>
2022-12-25 21:50:25 +0800

    Q-227: calculator_II.rb with two phase calc

--- calculator_II.rb
+++ calculator_II.rb
@@ -23,3 +23,34 @@ def compute(stack)
   stack.each {|token| (token == :+ || token == :-) ? op = token : result = [result, token].inject(op)}
   result
 end
+
+
+# Q-227: two phase solution
+# 24/dec/2022
+def calculate(s)
+  stack = []
+  op = nil
+  s.scan(/\w+|\+|-|\*|\//).each do |t|
+    if t =~ /\d/
+      if op == '*' || op == '/'
+        op = stack.pop
+        stack << [stack.pop, t.to_i].inject(op.to_sym)
+      else
+        stack << t.to_i
+      end
+    else
+      stack << t
+      op = t
+    end
+  end
+  result = 0
+  op = "+"
+  stack.each do |t|
+    if t == '+' || t == '-'
+      op = t
+    else
+      result = [result, t].inject(op.to_sym)
+    end
+  end
+  result
+end
