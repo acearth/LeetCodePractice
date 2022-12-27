c439cf524aa4392ff5152d8480f6b8a5150e6ce5
Arthur Ace <arthur4it@gmail.com>
2022-12-27 22:50:26 +0800

    Q-8: my_atoi.rb Solved in one pass

--- my_atoi.rb
+++ my_atoi.rb
@@ -1,29 +1,26 @@
-def work(str)
-  return 0 if str.length==0
-  return -1*work(str[1..-1]) if str[0]=='-'
-  return work(str[1..-1]) if str[0]=='+'
-  res=0
-  str.chars.each do |ch|
-    res+=ch.ord-'0'.ord
-    res*=10
+# Q-8: my atoi: parse topic
+# 27/dec/2022
+# @param {String} s
+# @return {Integer}
+def my_atoi(s)
+  i = 0
+  i += 1 while s[i] == ' '
+  sign = 1
+  if s[i] == '+' || s[i] == '-'
+    sign = -1 if s[i] == '-'
+    i += 1
   end
-  res/=10
-  res
-end
-
-def my_atoi(str)
-  str.strip!
-  return 0 if str.length<1
-  set=%w[+ - 0 1 2 3 4 5 6 7 8 9]
-  return 0 if not set.include? str[0]
-  str[1..-1].chars.each_with_index do |ch, i|
-    return check_range(work(str[0..i])) if ch.ord<48||ch.ord>57
+  result = 0
+  while i < s.size && s[i].ord >= '0'.ord && s[i].ord <= '9'.ord
+    result *= 10
+    result += s[i].ord - '0'.ord
+    i += 1
   end
-  check_range(work(str))
-end
-
-def check_range(n)
-  return -1*2**31 if n<-1*2**31
-  return 2**31-1 if n>2**31-1
-  n
+  result *= sign
+  if result < -1 * 2 ** 31
+    result = -1 * 2 ** 31
+  elsif result > 2 ** 31 - 1
+    result = 2 ** 31 - 1
+  end
+  result
 end
