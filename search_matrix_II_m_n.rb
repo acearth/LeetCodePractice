0d36295be6e2dd4eb35e14c44f841a4fc630a943
Arthur Ace <arthur4it@gmail.com>
2023-01-07 20:49:12 +0800

    Q-240: real divide and conquer solution for matrix binary search

--- search_matrix_II_m_n.rb
+++ search_matrix_II_m_n.rb
@@ -1,28 +1,29 @@
 # line by line and col by col scan but passed in ruby.
 # Acceptable but bad for training. Could be seen in interview.
 def search_matrix(matrix, target)
-  startLine=0
-  tailColumn=matrix[0].size
-  while startLine<matrix.size&&tailColumn>0
-    cur=matrix[startLine][tailColumn-1]
-    return true if cur==target
-    if cur<target
-      startLine+=1
+  startLine = 0
+  tailColumn = matrix[0].size
+  while startLine < matrix.size && tailColumn > 0
+    cur = matrix[startLine][tailColumn - 1]
+    return true if cur == target
+    if cur < target
+      startLine += 1
     else
-      tailColumn-=1
+      tailColumn -= 1
     end
   end
   return false
 end
-m=[
-  [1,   4,  7, 11, 15],
-  [2,   5,  8, 12, 19],
-  [3,   6,  9, 16, 22],
+
+m = [
+  [1, 4, 7, 11, 15],
+  [2, 5, 8, 12, 19],
+  [3, 6, 9, 16, 22],
   [10, 13, 14, 17, 24],
   [18, 21, 23, 26, 30]
 ]
-p search_matrix(m,5)
-p search_matrix(m,20)
+p search_matrix(m, 5)
+p search_matrix(m, 20)
 
 # Q-240: row ordered, column ordered
 def search_matrix(matrix, target)
@@ -52,3 +53,33 @@ end
 #   log(n) + log(n-1)+ ... + log(1)=log(n)*log(n-1)*...*log(1)
 #   =log(n!) , also < nlog(n).
 #   Rarely seen.
+
+# Q-240: speed up by removing checking from 3/4 grid to 2/4 :: vertical checking
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+  search_vertical(matrix, 0, matrix.size, 0, matrix.first.size, target)
+end
+
+def vertical_search_column(matrix, col, up, down, target)
+  while up < down
+    m = (up + down) / 2
+    return m if matrix[m][col] == target
+    if matrix[m][col] < target
+      up = m + 1
+    else
+      down = m
+    end
+  end
+  up
+end
+
+def search_vertical(matrix, up, down, left, right, target)
+  return false unless up < down && left < right
+  return false unless matrix[up][left] <= target && target <= matrix[down - 1][right - 1]
+  col = (left + right) / 2
+  row = vertical_search_column(matrix, col, up, down, target)
+  return true if row < down && matrix[row][col] == target
+  search_vertical(matrix, row, down, left, col, target) || search_vertical(matrix, up, row, col + 1, right, target)
+end
