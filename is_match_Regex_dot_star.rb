1585b7d579380bb20cb286f766523dd7762ed3a4
Arthur Ace <arthur4it@gmail.com>
2023-01-15 17:31:35 +0800

    Q-10: regex match recheck

--- is_match_Regex_dot_star.rb
+++ is_match_Regex_dot_star.rb
@@ -24,3 +24,29 @@ def match(str, p)
   end
   false
 end
+
+# Q-10: revised on 15/jan/2023
+# @param {String} s
+# @param {String} p
+# @return {Boolean}
+def is_match(s, p)
+  dp = (1 + s.size).times.map { [false] * (1 + p.size) }
+  dp[0][0] = true
+  p.size.times { |j| dp[0][j + 1] = j > 0 && p[j] == '*' && dp[0][j - 1] } # check-1: init
+  s.chars.each_with_index do |cs, i|
+    p.chars.each_with_index do |cp, j|
+      if cs == cp || cp == '.'
+        dp[i + 1][j + 1] = dp[i][j]
+      elsif cp == '*' # check-2: branch condition
+        if cs == p[j - 1] || p[j - 1] == '.'
+          # check-3: cases: "abd" v. "abc*d" & "abcd" v. "abc*d"
+          # Also, "abcc...d" v. "abc*d" is included by dp[i][j+1]
+          dp[i + 1][j + 1] = dp[i + 1][j - 1] || dp[i][j + 1]
+        else
+          dp[i + 1][j + 1] = dp[i + 1][j - 1]
+        end
+      end
+    end
+  end
+  dp.last.last
+end
